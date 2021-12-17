349624afc8c9b4a79cb87faf96a6a749fb51b093
artur <arturbv2003@gmail.com>
2021-12-17 13:54:29 -0300

    update do codigo de mateus
    fiz com que n desse pra agachar no ar, e ao agachar ele reduz a velocidade 1.5 vezes.

--- um-inicio-eu-acho.c
+++ um-inicio-eu-acho.c
@@ -1,27 +1,27 @@
 #include "raylib.h"
-#include "raymath.h"
+#define NUM_FRAMES 3
+#define PHYSAC_IMPLEMENTATION
+#include "extras/physac.h"
 
-#define G 400
-#define PLAYER_JUMP_SPD 350.0f
-#define PLAYER_HOR_SPD 300.0f
+#define VELOCITY 0.5f
 
-typedef struct Player {
-    Vector2 position;
-    float speed;
-    bool canJump;
-} Player;
+void abaixar(PhysicsBody *body){
+    Vector2 position = (*body)->position;
+    int isGrounded = (*body)->isGrounded;
+    DestroyPhysicsBody(*body);
 
-typedef struct EnvItem {
-    Rectangle rect;
-    int blocking;
-    Color color;
-} EnvItem;
+    *body = CreatePhysicsBodyRectangle(position, 50, 25, 1);
+    (*body)->isGrounded = isGrounded;
+}
 
+void aumentar(PhysicsBody *body){
+    Vector2 position = (*body)->position;
+    int isGrounded = (*body)->isGrounded;
+    DestroyPhysicsBody(*body);
 
-void UpdatePlayer(Player *player, EnvItem *envItems, int envItemsLength, float delta);
-
-void UpdateCameraCenter(Camera2D *camera, Player *player, EnvItem *envItems, int envItemsLength, float delta, int width, int height);
-
+    *body = CreatePhysicsBodyRectangle(position, 50, 50, 1);
+    (*body)->isGrounded = isGrounded;
+}
 
 int main(void)
 {
@@ -30,135 +30,204 @@ int main(void)
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example - 2d camera");
+    InitWindow(screenWidth, screenHeight, "raylib [textures] example - sprite button");
 
-    Player player = { 0 };
-    player.position = (Vector2){ 400, 280 };
-    player.speed = 0;
-    player.canJump = false;
-    EnvItem envItems[] = {
-        {{ 0, 0, 1000, 400 }, 0, LIGHTGRAY },
-        {{ 0, 400, 1000, 200 }, 1, GRAY },
-        {{ 300, 200, 400, 10 }, 1, GRAY },
-        {{ 250, 300, 100, 10 }, 1, GRAY },
-        {{ 650, 300, 100, 10 }, 1, GRAY }
-    };
+    InitAudioDevice(); // Initialize audio device
 
-    int envItemsLength = sizeof(envItems)/sizeof(envItems[0]);
+    Sound fxButton = LoadSound("resources/buttonfx.wav");     // Load button sound
+    Texture2D button = LoadTexture("resources/btn-bg.png");   // Load button texture
+    Texture2D texture = LoadTexture("resources/unknown.png"); // background texture
 
-    Camera2D camera = { 0 };
-    camera.target = player.position;
-    camera.offset = (Vector2){ screenWidth/2.0f, screenHeight/2.0f };
+    // Define frame rectangle for drawing
+    float frameHeight = (float)button.height / NUM_FRAMES;
+    Rectangle sourceRec = {0, 0, (float)button.width, frameHeight};
+
+    // Define button bounds on screen
+    Rectangle btnBounds = {screenWidth / 2.0f - button.width / 2.0f, screenHeight / 2.0f - button.height / NUM_FRAMES / 2.0f, (float)button.width, frameHeight};
+
+    int btnState = 0;       // Button state: 0-NORMAL, 1-MOUSE_HOVER, 2-PRESSED
+    bool btnAction = false; // Button action should be activated
+
+    Vector2 mousePoint = {0.0f, 0.0f};
+
+    //Game Configuration
+
+    // Initialize physics and default physics bodies
+    InitPhysics();
+
+    // Create floor and walls rectangle physics body
+    PhysicsBody floor = CreatePhysicsBodyRectangle((Vector2){screenWidth / 2.0f, (float)screenHeight}, (float)screenWidth * 5, 100, 10);
+    PhysicsBody platformLeft = CreatePhysicsBodyRectangle((Vector2){screenWidth * 0.25f, screenHeight * 0.6f}, screenWidth * 0.25f, 10, 10);
+    PhysicsBody platformRight = CreatePhysicsBodyRectangle((Vector2){screenWidth * 0.75f, screenHeight * 0.6f}, screenWidth * 0.25f, 10, 10);
+    PhysicsBody platformRight2 = CreatePhysicsBodyRectangle((Vector2){screenWidth * 1.25f, screenHeight * 0.8f}, 100, 80, 10);
+    PhysicsBody platformRight3 = CreatePhysicsBodyRectangle((Vector2){screenWidth * 1.50f, screenHeight * 0.7f}, 100, 20, 10);
+    PhysicsBody wallLeft = CreatePhysicsBodyRectangle((Vector2){-5, screenHeight / 2.0f}, 10, (float)screenHeight, 10);
+    PhysicsBody wallRight = CreatePhysicsBodyRectangle((Vector2){(float)screenWidth * 3 + 5, screenHeight / 2.0f}, 10, (float)screenHeight, 10);
+
+    // Disable dynamics to floor and walls physics bodies
+    floor->enabled = false;
+    platformLeft->enabled = false;
+    platformRight->enabled = false;
+    platformRight2->enabled = false;
+    platformRight3->enabled = false;
+    wallLeft->enabled = false;
+    wallRight->enabled = false;
+
+    int isShortened = 0;
+    int timeElapsed = 0;
+
+    // Create movement physics body
+    PhysicsBody body = CreatePhysicsBodyRectangle((Vector2){screenWidth / 2.0f, screenHeight / 2.0f}, 50, 50, 1);
+    PhysicsBody follower = CreatePhysicsBodyRectangle((Vector2){screenWidth / 2.0f, screenHeight / 2.0f}, 50, 50, 1);
+    body->freezeOrient = true; // Constrain body rotation to avoid little collision torque amounts
+    follower->freezeOrient = true;
+
+    Camera2D camera = {0};
+    camera.target = body->position;
+    camera.offset = (Vector2){screenWidth / 2.0f, screenHeight / 2.0f};
     camera.rotation = 0.0f;
-    camera.zoom = 1.0f;
-
-    // Store pointers to the multiple update camera functions
-    void (*cameraUpdaters[])(Camera2D*, Player*, EnvItem*, int, float, int, int) = {
-        UpdateCameraCenter,
-    };
-
-    int cameraOption = 0;
-    //int cameraUpdatersLength = sizeof(cameraUpdaters)/sizeof(cameraUpdaters[0]);
-
+    camera.zoom = 0.5f;
 
     SetTargetFPS(60);
     //--------------------------------------------------------------------------------------
 
-    // Main game loop
-    while (!WindowShouldClose())
+    while (!WindowShouldClose()) // Detect window close button or ESC key
     {
-        // Update
-        //----------------------------------------------------------------------------------
-        float deltaTime = GetFrameTime();
+        mousePoint = GetMousePosition();
+        btnAction = false;
 
-        UpdatePlayer(&player, envItems, envItemsLength, deltaTime);
-
-
-        if (IsKeyPressed(KEY_R))
+        // Check button state
+        if (CheckCollisionPointRec(mousePoint, btnBounds))
         {
-            camera.zoom = 1.0f;
-            player.position = (Vector2){ 400, 280 };
+            if (IsMouseButtonDown(MOUSE_BUTTON_LEFT))
+                btnState = 2;
+            else
+                btnState = 1;
+
+            if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT))
+                btnAction = true;
         }
+        else
+            btnState = 0;
 
-        //if (IsKeyPressed(KEY_C)) cameraOption = (cameraOption + 1)%cameraUpdatersLength;
-
-        // Call update camera function by its point
-        cameraUpdaters[cameraOption](&camera, &player, envItems, envItemsLength, deltaTime, screenWidth, screenHeight);
+        // Calculate button frame rectangle to draw depending on button state
+        sourceRec.y = btnState * frameHeight;
         //----------------------------------------------------------------------------------
 
-        // Draw
-        //----------------------------------------------------------------------------------
-        BeginDrawing();
+        //Check if button is clicked
+        if (btnAction)
+        {
+            PlaySound(fxButton);
+            UnloadTexture(button); // Unload button texture
 
-            ClearBackground(LIGHTGRAY);
+            //The game
+            while (!WindowShouldClose()) // Detect window close button or ESC key
+            {
+                camera.target = body->position;
+                // Update
+                //----------------------------------------------------------------------------------
+                UpdatePhysics(); // Update physics system
+                timeElapsed++;
+                if(isShortened && timeElapsed % 50 == 0){
+                    aumentar(&body);
+                    isShortened = 0;
+                    timeElapsed = 0;
+                }
 
-            BeginMode2D(camera);
+                if (IsKeyPressed(KEY_R)) // Reset physics input
+                {
+                    // Reset movement physics body position, velocity and rotation
+                    body->position = (Vector2){screenWidth / 2.0f, screenHeight / 2.0f};
+                    body->velocity = (Vector2){0, 0};
+                    SetPhysicsBodyRotation(body, 0);
+                }
 
-                for (int i = 0; i < envItemsLength; i++) DrawRectangleRec(envItems[i].rect, envItems[i].color);
+                if(IsKeyPressed(KEY_DOWN) && !isShortened && body->isGrounded){
+                    abaixar(&body);
+                    isShortened = 1;                   
+                }
 
-                Rectangle playerRect = { player.position.x - 20, player.position.y - 40, 40, 40 };
-                DrawRectangleRec(playerRect, RED);
+                follower->position = (Vector2){body->position.x - 500, body->position.y};
+
+                // Horizontal movement input
+                body->velocity.x = VELOCITY;
+                follower->velocity.x = VELOCITY;
                 
-            DrawText("Controles:", 20, 200, 10, BLACK);
-            DrawText("- left/right para mover", 20, 220, 10, DARKGRAY);
-            DrawText("- Space para pular", 40, 240, 10, DARKGRAY);
-            DrawText("- Down para dar rasteira", 40, 260, 10, DARKGRAY);
-            
+                if(isShortened == 1 && body->isGrounded){
+                    body->velocity.x = VELOCITY/1.5;
+                }
 
-            EndMode2D();
+                // Vertical movement input checking if player physics body is grounded
+                if (IsKeyDown(KEY_SPACE) && body->isGrounded)
+                    body->velocity.y = -VELOCITY * 4;
+                //----------------------------------------------------------------------------------
 
-        EndDrawing();
-        //----------------------------------------------------------------------------------
+                // Draw
+                //----------------------------------------------------------------------------------
+                BeginDrawing();
+
+                BeginMode2D(camera);
+
+                ClearBackground(BLACK);
+
+                DrawFPS(screenWidth - 90, screenHeight - 30);
+
+                // Draw created physics bodies
+                int bodiesCount = GetPhysicsBodiesCount();
+                for (int i = 0; i < bodiesCount; i++)
+                {
+                    PhysicsBody body = GetPhysicsBody(i);
+
+                    int vertexCount = GetPhysicsShapeVerticesCount(i);
+                    for (int j = 0; j < vertexCount; j++)
+                    {
+                        // Get physics bodies shape vertices to draw lines
+                        // Note: GetPhysicsShapeVertex() already calculates rotation transformations
+                        Vector2 vertexA = GetPhysicsShapeVertex(body, j);
+
+                        int jj = (((j + 1) < vertexCount) ? (j + 1) : 0); // Get next vertex or first to close the shape
+                        Vector2 vertexB = GetPhysicsShapeVertex(body, jj);
+
+                        DrawLineV(vertexA, vertexB, GREEN); // Draw a line between two vertex positions
+                    }
+                }
+
+                DrawText("Use 'ARROWS' to move player", 10, 10, 10, WHITE);
+                DrawText("Press 'R' to reset example", 10, 30, 10, WHITE);
+
+                EndDrawing();
+                //----------------------------------------------------------------------------------
+            }
+            ClosePhysics();       // Unitialize physics
+
+            CloseAudioDevice(); // Close audio device
+
+            CloseWindow(); // Close window and OpenGL context
+        }
+        else
+        {
+
+            // Draw
+            //----------------------------------------------------------------------------------
+            BeginDrawing();
+
+            ClearBackground(RAYWHITE);
+            DrawTexture(texture, 0, 0, WHITE);
+
+            DrawTextureRec(button, sourceRec, (Vector2){btnBounds.x, btnBounds.y}, WHITE); // Draw button frame
+            DrawText("INICIAR O JOGO", 200, 100, 50, WHITE);
+
+            DrawText("Espaço: Pular", 300, 100, 50, WHITE);
+
+            EndDrawing();
+            //----------------------------------------------------------------------------------
+        }
     }
 
-    // De-Initialization
-    //--------------------------------------------------------------------------------------
-    CloseWindow();        // Close window and OpenGL context
+    CloseAudioDevice(); // Close audio device
+
+    CloseWindow(); // Close window and OpenGL context
     //--------------------------------------------------------------------------------------
 
     return 0;
-}
-
-void UpdatePlayer(Player *player, EnvItem *envItems, int envItemsLength, float delta)
-{
-    if (IsKeyDown(KEY_DOWN) && IsKeyDown(KEY_LEFT) && player->canJump == true) player->position.x -= PLAYER_HOR_SPD*delta/2;
-    else if (IsKeyDown(KEY_DOWN) && IsKeyDown(KEY_RIGHT) && player->canJump == true) player->position.x += PLAYER_HOR_SPD*delta/2;
-    else if (IsKeyDown(KEY_LEFT)) player->position.x -= PLAYER_HOR_SPD*delta;
-    else if (IsKeyDown(KEY_RIGHT)) player->position.x += PLAYER_HOR_SPD*delta;
-    if (IsKeyDown(KEY_SPACE) && player->canJump)
-    {
-        player->speed = -PLAYER_JUMP_SPD;
-        player->canJump = false;
-    }
-
-    int hitObstacle = 0;
-    for (int i = 0; i < envItemsLength; i++)
-    {
-        EnvItem *ei = envItems + i;
-        Vector2 *p = &(player->position);
-        if (ei->blocking &&
-            ei->rect.x <= p->x &&
-            ei->rect.x + ei->rect.width >= p->x &&
-            ei->rect.y >= p->y &&
-            ei->rect.y < p->y + player->speed*delta)
-        {
-            hitObstacle = 1;
-            player->speed = 0.0f;
-            p->y = ei->rect.y;
-        }
-    }
-
-    if (!hitObstacle)
-    {
-        player->position.y += player->speed*delta;
-        player->speed += G*delta;
-        player->canJump = false;
-    }
-    else player->canJump = true;
-}
-
-void UpdateCameraCenter(Camera2D *camera, Player *player, EnvItem *envItems, int envItemsLength, float delta, int width, int height)
-{
-    camera->offset = (Vector2){ width/2.0f, height/2.0f };
-    camera->target = player->position;
 }
